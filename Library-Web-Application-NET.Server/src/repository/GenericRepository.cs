cb68c8b273a5c1e1cb613fe45e97cbafb3c4468f
gszp1 <grzybekmateusz@poczta.fm>
2024-07-15 22:30:55 +0200

    Adds Generic repository.

---
+++ Library-Web-Application-NET.Server/src/repository/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using Library_Web_Application_NET.Server.src.data.context;
+
+namespace Library_Web_Application_NET.Server.src.repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly LibraryDbContext context;
+
+        public GenericRepository(LibraryDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            context.Set<T>().AddRange(entities);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void Remove(T entity)
+        {
+            context.Set<T>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            context.Set<T>().RemoveRange(entities);
+        }
+    }
+}
---
+++ Library-Web-Application-NET.Server/src/repository/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Library_Web_Application_NET.Server.src.repository
+{
+    public interface IGenericRepository<T> where T: class
+    {
+        T GetById(int id);
+        
+        IEnumerable<T> GetAll();
+
+        void Add(T entity);
+
+        void AddRange(IEnumerable<T> entities);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
