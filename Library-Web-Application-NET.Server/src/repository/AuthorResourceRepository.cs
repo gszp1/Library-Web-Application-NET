ac2607142cd3698513aa51760b9b3d1a630f70b1
gszp1 <grzybekmateusz@poczta.fm>
2024-07-16 22:32:55 +0200

    Adds AuthorResourceRepository.

---
+++ Library-Web-Application-NET.Server/src/repository/AuthorResourceRepository.cs
@@ -0,0 +1,12 @@
+﻿using Library_Web_Application_NET.Server.src.data.context;
+using Library_Web_Application_NET.Server.src.model;
+
+namespace Library_Web_Application_NET.Server.src.repository
+{
+    public class AuthorResourceRepository : GenericRepository<AuthorResource>
+    {
+        public AuthorResourceRepository(LibraryDbContext context) : base(context)
+        {
+        }
+    }
+}
