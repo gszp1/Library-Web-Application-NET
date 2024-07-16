15075f03e20721ecb9ae02770464e0f9e5935d08
gszp1 <grzybekmateusz@poczta.fm>
2024-07-16 22:26:54 +0200

    Adds ResourceRepository.

---
+++ Library-Web-Application-NET.Server/src/repository/ResourceRepository.cs
@@ -0,0 +1,12 @@
+﻿using Library_Web_Application_NET.Server.src.data.context;
+using Library_Web_Application_NET.Server.src.model;
+
+namespace Library_Web_Application_NET.Server.src.repository
+{
+    public class ResourceRepository : GenericRepository<Resource>
+    {
+        public ResourceRepository(LibraryDbContext context) : base(context)
+        {
+        }
+    }
+}
