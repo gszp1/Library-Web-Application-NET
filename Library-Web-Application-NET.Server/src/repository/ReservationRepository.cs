99648d90e5f4f197df1c99086aadb457c920962c
gszp1 <grzybekmateusz@poczta.fm>
2024-07-16 22:30:24 +0200

    Adds ReservationRepository.

---
+++ Library-Web-Application-NET.Server/src/repository/ReservationRepository.cs
@@ -0,0 +1,12 @@
+﻿using Library_Web_Application_NET.Server.src.data.context;
+using Library_Web_Application_NET.Server.src.model;
+
+namespace Library_Web_Application_NET.Server.src.repository
+{
+    public class ReservationRepository : GenericRepository<Reservation>
+    {
+        public ReservationRepository(LibraryDbContext context) : base(context)
+        {
+        }
+    }
+}
