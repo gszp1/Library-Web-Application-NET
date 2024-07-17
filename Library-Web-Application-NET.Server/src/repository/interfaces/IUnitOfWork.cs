54a3e1c9ac956d3947e3c9c39e2c1f4c3fb90764
gszp1 <grzybekmateusz@poczta.fm>
2024-07-17 22:58:06 +0200

    Adds IUnitOfWork interface.

---
+++ Library-Web-Application-NET.Server/src/repository/interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿namespace Library_Web_Application_NET.Server.src.repository.interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAuthorRepository Authors { get; }
+        
+        IAuthorResourceRepository AuthorResources { get; }
+
+        IPublisherRepository Publishers { get; }
+
+        IReservationRepository Reservations { get; }
+
+        IResourceInstanceRepository ResourceInstances { get; }
+
+        IResourceRepository Resources { get; }
+
+        int Complete();
+    }
+}
