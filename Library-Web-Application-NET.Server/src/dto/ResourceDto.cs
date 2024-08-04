55f33231f936ab999b7a64852038d4adeee41ffe
gszp1 <grzybekmateusz@poczta.fm>
2024-08-04 21:29:53 +0200

    Adds ResourceDto.

---
+++ Library-Web-Application-NET.Server/src/dto/ResourceDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Library_Web_Application_NET.Server.src.dto
+{
+    public class ResourceDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Identifier { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Publisher { get; set; }
+
+        public List<AuthorDto> Authors { get; set; } = [];
+    }
+}
