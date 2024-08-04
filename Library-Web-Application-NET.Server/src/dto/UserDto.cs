acb4e67d989887f3d26e8948afb7981cc727fff1
gszp1 <grzybekmateusz@poczta.fm>
2024-08-04 21:11:09 +0200

    Adds UserDto.

--- Library-Web-Application-NET.Server/src/dto/UserDto.cs
+++ Library-Web-Application-NET.Server/src/dto/UserDto.cs
@@ -2,5 +2,16 @@
 {
     public class UserDto
     {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateOnly JoinDate { get; set; }
+
+        public string Email { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
