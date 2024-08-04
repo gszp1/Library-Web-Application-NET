ba6ccf62fe5fceefd4b000f94892f15219cad3de
gszp1 <grzybekmateusz@poczta.fm>
2024-08-04 22:19:09 +0200

    Adds PublisherDto.

---
+++ Library-Web-Application-NET.Server/src/dto/PublisherDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Library_Web_Application_NET.Server.src.dto
+{
+    public class PublisherDto
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
