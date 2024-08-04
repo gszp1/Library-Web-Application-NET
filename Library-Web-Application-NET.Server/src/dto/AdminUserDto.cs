8a253ae6d4f3ffb08c804582224db536f3992b55
gszp1 <grzybekmateusz@poczta.fm>
2024-08-04 22:52:27 +0200

    Adds AdminUserDto.

---
+++ Library-Web-Application-NET.Server/src/dto/AdminUserDto.cs
@@ -0,0 +1,25 @@
+﻿using Library_Web_Application_NET.Server.src.util;
+
+namespace Library_Web_Application_NET.Server.src.dto
+{
+    public class AdminUserDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateOnly JoinDate { get; set; }
+
+        public string Email { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public UserStatus Status { get; set; }
+
+        public Role Role { get; set; }  
+    }
+}
