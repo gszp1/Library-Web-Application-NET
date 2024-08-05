0f863f8a7554b13f048d7ce757f3aefc6185f610
gszp1 <grzybekmateusz@poczta.fm>
2024-08-05 20:45:33 +0200

    Adds methods definitions to PublisherController.

--- Library-Web-Application-NET.Server/src/controller/PublisherController.cs
+++ Library-Web-Application-NET.Server/src/controller/PublisherController.cs
@@ -1,4 +1,5 @@
-﻿using Library_Web_Application_NET.Server.src.service;
+﻿using Library_Web_Application_NET.Server.src.dto;
+using Library_Web_Application_NET.Server.src.service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library_Web_Application_NET.Server.src.controller
@@ -12,6 +13,24 @@ namespace Library_Web_Application_NET.Server.src.controller
         public PublisherController(PublisherService publisherService)
         {
             this.publisherService = publisherService;
+        }
+
+        [HttpPost("create")]
+        public ActionResult<string> Create([FromBody] PublisherDto dto)
+        {
+
+        }
+
+        [HttpGet("all")]
+        public ActionResult<List<AdminPublisherDto>> GetAll()
+        {
+
+        }
+
+        [HttpPut("update")]
+        public ActionResult<string> Update([FromBody] AdminPublisherDto dto)
+        {
+
         }
     }
 }
