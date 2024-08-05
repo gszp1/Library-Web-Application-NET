96168beb1be3bb0dc7d3c1d696023fb874eea920
gszp1 <grzybekmateusz@poczta.fm>
2024-08-05 22:18:02 +0200

    Adds methods definitions in ResourceInstanceController.

--- Library-Web-Application-NET.Server/src/controller/ResourceInstanceController.cs
+++ Library-Web-Application-NET.Server/src/controller/ResourceInstanceController.cs
@@ -1,4 +1,5 @@
-﻿using Library_Web_Application_NET.Server.src.service;
+﻿using Library_Web_Application_NET.Server.src.dto;
+using Library_Web_Application_NET.Server.src.service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library_Web_Application_NET.Server.src.controller
@@ -12,6 +13,31 @@ namespace Library_Web_Application_NET.Server.src.controller
         public ResourceInstanceController(ResourceInstanceService resourceInstanceService)
         {
             this.resourceInstanceService = resourceInstanceService;
+        }
+
+
+        [HttpGet("all/resource/{id}")]
+        public ActionResult<List<AdminInstanceDto>> GetAllForResource(int id)
+        {
+
+        }
+
+        [HttpGet("update")]
+        public ActionResult<string> UpdateInstance([FromBody] AdminInstanceDto dto)
+        {
+
+        }
+
+        [HttpPut("{id}/withdraw")]
+        public ActionResult<string> WithdrawInstance(int id)
+        {
+
+        }
+
+        [HttpPost("create/{id}")]
+        public ActionResult<string> CreateInstance(int id)
+        {
+
         }
     }
 }
