20fc78edeceeb11516c88f1006383f8d741d39b7
gszp1 <grzybekmateusz@poczta.fm>
2024-08-05 22:03:38 +0200

    Adds methods definitions in ResourceController.

--- Library-Web-Application-NET.Server/src/controller/ResourceController.cs
+++ Library-Web-Application-NET.Server/src/controller/ResourceController.cs
@@ -1,4 +1,5 @@
-﻿using Library_Web_Application_NET.Server.src.service;
+﻿using Library_Web_Application_NET.Server.src.dto;
+using Library_Web_Application_NET.Server.src.service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library_Web_Application_NET.Server.src.controller
@@ -13,5 +14,48 @@ namespace Library_Web_Application_NET.Server.src.controller
         {
             this.resourceService = resourceService;
         }
+
+        [HttpGet("all")]
+        public ActionResult<List<ResourceDto>> GetAll([FromQuery] string? keyword)
+        {
+
+        }
+
+        [HttpGet("admin/all")]
+        public ActionResult<List<AdminResourceDto>> GetAllAdmin()
+        {
+
+        }
+
+        [HttpGet("all/paginated")]
+        public ActionResult<List<ResourceDto>> GetAllPaginated([FromQuery] string? keyword)
+        {
+            // Do proper pagination
+        }
+
+        [HttpGet("{id}/description")]
+        public ActionResult<ResourceDescriptionDto> GetDescription(int id) 
+        {
+        
+        }
+
+        [HttpGet("{id}/instances/notReserved")]
+        public ActionResult<List<InstanceDto>> GetNotReservedInstances(int id)
+        {
+
+        }
+
+        [HttpPost("create")]
+        public ActionResult<string> CreateResource([FromBody] CreateResourceDto dto)
+        {
+
+        }
+
+        [HttpPut("update")]
+        public ActionResult<string> UpdateResource([FromBody] UpdateResourceDto dto)
+        {
+
+        }
+
     }
 }
