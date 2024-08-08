a96d520e183e3599611dae9c68447d597f079daa
gszp1 <grzybekmateusz@poczta.fm>
2024-08-08 21:53:23 +0200

    Adds methods for IPublisherRepository>

--- Library-Web-Application-NET.Server/src/repository/interfaces/IPublisherRepository.cs
+++ Library-Web-Application-NET.Server/src/repository/interfaces/IPublisherRepository.cs
@@ -4,5 +4,8 @@ namespace Library_Web_Application_NET.Server.src.repository.interfaces
 {
     public interface IPublisherRepository : IGenericRepository<Publisher>
     {
+        Task<Publisher> FindByNameAsync(string name);
+
+        Task<Publisher> FindByAddressAsync(string address);
     }
 }
