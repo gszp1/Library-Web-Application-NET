f104e43bcf726611dfc2e50dcd8be2270e4458bd
gszp1 <grzybekmateusz@poczta.fm>
2024-08-08 22:55:19 +0200

    Adds ResourceInstanceRepository methods.

--- Library-Web-Application-NET.Server/src/repository/interfaces/IResourceInstanceRepository.cs
+++ Library-Web-Application-NET.Server/src/repository/interfaces/IResourceInstanceRepository.cs
@@ -4,5 +4,10 @@ namespace Library_Web_Application_NET.Server.src.repository.interfaces
 {
     public interface IResourceInstanceRepository : IGenericRepository<ResourceInstance>
     {
+        Task<List<ResourceInstance>> FindByResourceIdAndIsReservedFalseAsync(int resourceId);
+
+        Task<List<ResourceInstance>> FindByResourceIdAsync(int resourceId, string sort);
+
+        Task<ResourceInstance> FindByResourceIdAsync(int resourceId);
     }
 }
