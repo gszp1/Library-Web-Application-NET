59f76fb1d4f88bb54e5cdfda47b58d639c458fe4
gszp1 <grzybekmateusz@poczta.fm>
2024-08-09 21:51:17 +0200

    Adds IUserRepository interface.

---
+++ Library-Web-Application-NET.Server/src/repository/interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Library_Web_Application_NET.Server.src.model;
+
+namespace Library_Web_Application_NET.Server.src.repository.interfaces
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        Task<User> FindByEmailAsync(string email);
+
+        Task<List<User>> FindByEmailKeywordAsync(string keyword);
+
+        Task<long> CountUsersAsync();
+
+        Task<long> CountByRegistrationDateMonthAsync(int month);
+    }
+}
