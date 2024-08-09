3cf206daf44b451186c6d03bc9ea78ffa54f483e
gszp1 <grzybekmateusz@poczta.fm>
2024-08-09 21:45:34 +0200

    Adds methods for IResourceRepository.

--- Library-Web-Application-NET.Server/src/repository/interfaces/IResourceRepository.cs
+++ Library-Web-Application-NET.Server/src/repository/interfaces/IResourceRepository.cs
@@ -4,5 +4,14 @@ namespace Library_Web_Application_NET.Server.src.repository.interfaces
 {
     public interface IResourceRepository : IGenericRepository<Resource>
     {
+        // FindAllPageableAsync
+
+        Task<List<Resource>> FindAllByTitleKeywordAsync(string keyword);
+
+        Task<Resource> FindByTitleAsync(string title);
+
+        Task<Resource> FindByIdentifierAsync(string identifier);
+
+        Task<Resource> FindAllPageable();
     }
 }
