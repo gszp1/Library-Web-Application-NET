a09e277b4e1da40f07354fd2ed9e17a4b7aef806
gszp1 <grzybekmateusz@poczta.fm>
2024-08-09 21:54:05 +0200

    Adds UserRepository class.

---
+++ Library-Web-Application-NET.Server/src/repository/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Library_Web_Application_NET.Server.src.data.context;
+using Library_Web_Application_NET.Server.src.model;
+using Library_Web_Application_NET.Server.src.repository.interfaces;
+
+namespace Library_Web_Application_NET.Server.src.repository
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(LibraryDbContext context) : base(context) { }
+    }
+}
