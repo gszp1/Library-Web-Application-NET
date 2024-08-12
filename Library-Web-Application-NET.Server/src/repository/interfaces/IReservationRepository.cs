d3cb5521ba71e77de6716fae77e2320779b6985c
gszp1 <grzybekmateusz@poczta.fm>
2024-08-12 23:23:33 +0200

    Adds methods to IReservationRepository.

--- Library-Web-Application-NET.Server/src/repository/interfaces/IReservationRepository.cs
+++ Library-Web-Application-NET.Server/src/repository/interfaces/IReservationRepository.cs
@@ -5,21 +5,28 @@ namespace Library_Web_Application_NET.Server.src.repository.interfaces
 {
     public interface IReservationRepository : IGenericRepository<Reservation>
     {
-        Task<int> CountResourceReservationsWithStatusAsync
-        (
+        Task<int> CountResourceReservationsWithStatusAsync(
             int resourceId,
             string userEmail,
             List<ReservationStatus> statuses
         );
 
-        Task<List<Reservation>> FindAllByReservationStatusWithInstancesAsync
-        (
+        Task<List<Reservation>> FindAllByReservationStatusWithInstancesAsync(
             List<ReservationStatus> statuses
+        );
+
+        Task<Reservation> FindByReservationIdWithInstanceAsync(int reservationId);
+
+        Task<List<Reservation>> FindAllByUserEmailWithInstancesAsync(string userEmail);
+
+        Task<List<Reservation>> FindAllByUserEmailAndReservationStatusWithInstancesAsync(
+            string email,
+            ReservationStatus status
         );
 
         Task<List<Reservation>> FindAllWithDataAsync(string sortOrder);
 
-        Task<long> CountReservationWithStatusAsync(ReservationStatus status);
+        Task<long> CountReservationsWithStatusAsync(ReservationStatus status);
 
         Task<long> CountReservationsByStartMonthAsync(int month);
 
