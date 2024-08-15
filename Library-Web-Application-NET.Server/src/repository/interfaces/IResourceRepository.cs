de62447222d94935211b826a321e71cde7c5cc56
gszp1 <grzybekmateusz@poczta.fm>
2024-08-15 14:31:24 +0200

    Adds methods implementation to ResourceRepository.

--- Library-Web-Application-NET.Server/src/repository/interfaces/IResourceRepository.cs
+++ Library-Web-Application-NET.Server/src/repository/interfaces/IResourceRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Library_Web_Application_NET.Server.src.model;
+using Library_Web_Application_NET.Server.src.util;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Library_Web_Application_NET.Server.src.repository.interfaces
 {
@@ -6,18 +8,18 @@ namespace Library_Web_Application_NET.Server.src.repository.interfaces
     {
         Task<List<Resource>> FindAllWithAuthorsAsync();
 
-        // FindAllWithAuthorsPageableAsync
+        Task<PageResult<Resource>> FindAllWithAuthorsPageableAsync(Pageable pageable);
 
-        Task<Resource> FindByResourceIdAsync(int resourceId);
+        Task<Resource?> FindByResourceIdAsync(int resourceId);
 
         Task<List<Resource>> FindAllByTitleKeywordAsync(string keyword);
 
-        // FindAllByTitleKeywordPageableAsync
+        Task<PageResult<Resource>> FindAllByTitleKeywordPageableAsync(string keyword, Pageable pageable);
 
-        Task<Resource> FindByTitleAsync(string title);
+        Task<Resource?> FindByTitleAsync(string title);
 
-        Task<Resource> FindByIdentifierAsync(string identifier);
+        Task<Resource?> FindByIdentifierAsync(string identifier);
 
-        Task<List<Resource>> FinAllWithDataAsync(string sortOrder);
+        Task<List<Resource>> FinAllWithDataAsync(string sortBy, bool descending);
     }
 }
