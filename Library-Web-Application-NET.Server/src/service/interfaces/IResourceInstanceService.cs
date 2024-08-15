4b081bf2512ffceefa0d0dac6e8899a7581f6992
gszp1 <grzybekmateusz@poczta.fm>
2024-08-15 19:23:07 +0200

    Adds methods to IResourceInstanceService.'

--- Library-Web-Application-NET.Server/src/service/interfaces/IResourceInstanceService.cs
+++ Library-Web-Application-NET.Server/src/service/interfaces/IResourceInstanceService.cs
@@ -1,8 +1,15 @@
-﻿using Library_Web_Application_NET.Server.src.model;
+﻿using Library_Web_Application_NET.Server.src.dto;
+using Library_Web_Application_NET.Server.src.model;
 
 namespace Library_Web_Application_NET.Server.src.service.interfaces
 {
     public interface IResourceInstanceService : IGenericService<ResourceInstance> 
     {
+        Task<List<InstanceDto>> GetNotReservedInstancesOfResourceAsync(int resourceId);
+
+        Task<ResourceInstance> GetResourceInstanceByIdAsync(int instanceId);
+
+        Task<ResourceInstance> SaveResourceInstanceAsync(ResourceInstance resourceInstance);
+
     }
 }
