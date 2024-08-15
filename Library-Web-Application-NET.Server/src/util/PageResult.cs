012ed92ebd1ae6f142544b098d6b2eca0979350b
gszp1 <grzybekmateusz@poczta.fm>
2024-08-15 13:06:05 +0200

    Adds PageResult class.

---
+++ Library-Web-Application-NET.Server/src/util/PageResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Library_Web_Application_NET.Server.src.util
+{
+    public class PageResult<T>
+    {
+        public List<T> Items { get; set; }
+        
+        public int TotalItems { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalPages => (int)Math.Ceiling(TotalItems / (double)PageSize);
+    }
+}
