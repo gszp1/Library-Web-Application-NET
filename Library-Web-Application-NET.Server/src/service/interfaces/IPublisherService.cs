441af4e67c40e8ef1cde9e2eac326923b4aac95d
gszp1 <grzybekmateusz@poczta.fm>
2024-08-15 18:51:31 +0200

    Adds methods to IPublisherService.

--- Library-Web-Application-NET.Server/src/service/interfaces/IPublisherService.cs
+++ Library-Web-Application-NET.Server/src/service/interfaces/IPublisherService.cs
@@ -1,8 +1,15 @@
-﻿using Library_Web_Application_NET.Server.src.model;
+﻿using Library_Web_Application_NET.Server.src.dto;
+using Library_Web_Application_NET.Server.src.model;
 
 namespace Library_Web_Application_NET.Server.src.service.interfaces
 {
     public interface IPublisherService : IGenericService<Publisher>
     {
+        Task CreatePublisher(PublisherDto dto);
+
+        Task<List<AdminPublisherDto>> GetAllPublishers();
+
+        Task UpdatePublisher(AdminPublisherDto dto);
     }
+
 }
