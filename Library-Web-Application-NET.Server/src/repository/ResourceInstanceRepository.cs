559506bd8a4b070d8c6cdb7c25b0e57dbb732f8f
gszp1 <grzybekmateusz@poczta.fm>
2024-08-15 20:03:43 +0200

    Adds methods to ResourceInstanceRepository.

--- Library-Web-Application-NET.Server/src/repository/ResourceInstanceRepository.cs
+++ Library-Web-Application-NET.Server/src/repository/ResourceInstanceRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Library_Web_Application_NET.Server.src.data.context;
 using Library_Web_Application_NET.Server.src.model;
 using Library_Web_Application_NET.Server.src.repository.interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Library_Web_Application_NET.Server.src.repository
 {
@@ -8,6 +10,81 @@ namespace Library_Web_Application_NET.Server.src.repository
     {
         public ResourceInstanceRepository(LibraryDbContext context) : base(context)
         {
+        }
+
+        public async Task<List<ResourceInstance>> FindByResourceIdAndIsReservedFalseAsync(int resourceId)
+        {
+            return await context
+                .ResourcesInstances
+                .Where(ri => ri.Reserved == false && ri.Resource.ResourceId == resourceId)
+                .ToListAsync();
+        }
+
+        public async Task<List<ResourceInstance>> FindByResourceIdAsync(int resourceId, string sortBy, bool descending)
+        {
+            var query = context
+                .ResourcesInstances
+                .Where(r => r.Resource.ResourceId == resourceId)
+                .AsQueryable();
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                var param = Expression.Parameter(typeof(ResourceInstance), "r");
+                var property = typeof(ResourceInstance).GetProperty(sortBy);
+                if (property != null)
+                {
+                    var sortExpression = Expression.Lambda(Expression.Property(param, property), param);
+
+                    var methodName = descending ? "OrderByDescending" : "OrderBy";
+
+                    var resultExpression = Expression.Call(
+                        typeof(Queryable),
+                        methodName,
+                        new Type[] { typeof(ResourceInstance), property.PropertyType },
+                        query.Expression,
+                        sortExpression);
+                    query = query.Provider.CreateQuery<ResourceInstance>(resultExpression);
+                }
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<List<ResourceInstance>> FindByResourceIdWithData(int resourceId, string sortBy, bool descending)
+        {
+            var query = context
+                .ResourcesInstances
+                .Where(ri => ri.Resource.ResourceId == resourceId)
+                .Include(ri => ri.Resource)
+                .Include(ri => ri.Reservations)
+                .AsQueryable();
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                var param = Expression.Parameter(typeof(ResourceInstance), "r");
+                var property = typeof(ResourceInstance).GetProperty(sortBy);
+                if (property != null)
+                {
+                    var sortExpression = Expression.Lambda(Expression.Property(param, property), param);
+
+                    var methodName = descending ? "OrderByDescending" : "OrderBy";
+
+                    var resultExpression = Expression.Call(
+                        typeof(Queryable),
+                        methodName,
+                        new Type[] { typeof(ResourceInstance), property.PropertyType },
+                        query.Expression,
+                        sortExpression);
+                    query = query.Provider.CreateQuery<ResourceInstance>(resultExpression);
+                }
+            }
+            return await query.ToListAsync();
+        }
+            
+        public async Task<ResourceInstance?> FindByIdWithData(int id)
+        {
+            return await context
+                .ResourcesInstances
+                .Include(ri => ri.Resource)
+                .Include(ri => ri.Reservations)
+                .FirstOrDefaultAsync(ri => ri.InstanceId == id);
         }
     }
 }
