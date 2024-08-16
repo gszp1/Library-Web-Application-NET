f3516f3ab4f543b074cb711af193da0295f4c158
gszp1 <grzybekmateusz@poczta.fm>
2024-08-16 21:39:46 +0200

    Adds methods for IResourceInstanceService.

--- Library-Web-Application-NET.Server/src/service/interfaces/IResourceInstanceService.cs
+++ Library-Web-Application-NET.Server/src/service/interfaces/IResourceInstanceService.cs
@@ -11,5 +11,18 @@ namespace Library_Web_Application_NET.Server.src.service.interfaces
 
         Task<ResourceInstance> SaveResourceInstanceAsync(ResourceInstance resourceInstance);
 
+        Task<List<InstanceDto>> GetAllInstancesByResourceIdAsync(int resourceId, string sortBy, bool descending);
+
+        Task<List<AdminInstanceDto>> GetAllAdminInstancesByResourceIdAsync(int id, string sortBy, bool descending);
+
+        Task<List<ResourceInstance>> SaveAllAsync(List<ResourceInstance> resourceInstances);
+
+        Task<List<ResourceInstance>> SaveAsync(ResourceInstance resourceInstance);
+
+        Task WithdrawInstanceAsync(int id);
+
+        Task UpdateInstanceAsync(AdminInstanceDto dto);
+
+        Task CreateInstanceAsync(int resourceId);
     }
 }
