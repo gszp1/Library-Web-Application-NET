9c9efc9711ad6a199d0f9c400ba377ad4729b40c
gszp1 <grzybekmateusz@poczta.fm>
2024-08-16 22:57:39 +0200

    Adds methods to IResourceService.

--- Library-Web-Application-NET.Server/src/service/ResourceService.cs
+++ Library-Web-Application-NET.Server/src/service/ResourceService.cs
@@ -5,5 +5,6 @@ namespace Library_Web_Application_NET.Server.src.service
 {
     public class ResourceService : GenericService<Resource>, IResourceService
     {
+
     }
 }
--- Library-Web-Application-NET.Server/src/service/interfaces/IResourceService.cs
+++ Library-Web-Application-NET.Server/src/service/interfaces/IResourceService.cs
@@ -1,8 +1,31 @@
-﻿using Library_Web_Application_NET.Server.src.model;
+﻿using Library_Web_Application_NET.Server.src.dto;
+using Library_Web_Application_NET.Server.src.model;
+using Library_Web_Application_NET.Server.src.util;
 
 namespace Library_Web_Application_NET.Server.src.service.interfaces
 {
     public interface IResourceService : IGenericService<Resource>
     {
+        Task<List<ResourceDto>> GetAllWithAuthorsAsync();
+
+        Task<PagedResult<ResourceDto>> GetAllWithAuthorsPageableAsync(Pageable pageable);
+
+        Task<ResourceDescriptionDto> GetResourceDescriptionAsync(int id);
+
+        Task<List<ResourceDto>> GetResourcesWithKeywordInTitleAsync(string keyword);
+
+        Task<PagedResult<ResourceDto>> GetResourcesWithKeywordInTitlePageableAsync(string keyword, Pageable pageable);
+
+        Task UpdateResourceImageAsync(int id, string url);
+
+        Task<bool> ResourceExistsAsync(int id);
+
+        Task<Resource> CreateResourceAsync(CreateResourceDto dto);
+
+        Task<List<AdminResourceDto>> GetAllAdminAsync();
+
+        Task UpdateResourceAsync(UpdateResourceDto dto);
+
+
     }
 }
