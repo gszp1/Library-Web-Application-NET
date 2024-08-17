44358ea2bc5b0131dd7433ea43126a34eaee471d
gszp1 <grzybekmateusz@poczta.fm>
2024-08-17 08:57:25 +0200

    Changes name of InvalidDataException to InvalidRequestDataException and adds constructor.

--- Library-Web-Application-NET.Server/src/exception/InvalidDataException.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Library_Web_Application_NET.Server.src.exception
-{
-    public class InvalidDataException : Exception
-    {
-    }
-}
--- Library-Web-Application-NET.Server/src/service/AuthorService.cs
+++ Library-Web-Application-NET.Server/src/service/AuthorService.cs
@@ -1,9 +1,22 @@
-﻿using Library_Web_Application_NET.Server.src.model;
+﻿using Library_Web_Application_NET.Server.src.dto;
+using Library_Web_Application_NET.Server.src.exception;
+using Library_Web_Application_NET.Server.src.model;
 using Library_Web_Application_NET.Server.src.service.interfaces;
 
 namespace Library_Web_Application_NET.Server.src.service
 {
     public class AuthorService : GenericService<Author>, IAuthorService
     {
+        public async Task CreateAuthorAsync(AdminAuthorDto dto)
+        {
+            if (dto.FirstName == null || dto.LastName == null || dto.Email == null)
+            {
+                throw new InvalidDataException("");
+            }
+        }
+
+        Task<List<FullAuthorDto>> GetAllAuthorsAsync();
+
+        Task UpdateAuthorAsync(FullAuthorDto dto);
     }
 }
