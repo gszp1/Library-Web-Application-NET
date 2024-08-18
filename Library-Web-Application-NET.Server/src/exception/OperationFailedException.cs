b8d0b5ed5b9241162857197de32b4185fde65c18
gszp1 <grzybekmateusz@poczta.fm>
2024-08-18 13:14:38 +0200

    Adds OperationFailedException.

---
+++ Library-Web-Application-NET.Server/src/exception/OperationFailedException.cs
@@ -0,0 +1,7 @@
+﻿namespace Library_Web_Application_NET.Server.src.data.config
+{
+    public class OperationFailedException : Exception
+    {
+        public OperationFailedException(string message) : base(message){}
+    }
+}
