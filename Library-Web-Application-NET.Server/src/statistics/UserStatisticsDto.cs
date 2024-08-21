7cf54d785512c3ae8b5d8b7c341248be201427c2
gszp1 <grzybekmateusz@poczta.fm>
2024-08-21 23:14:23 +0200

    Adds UserStatisticsDto.

---
+++ Library-Web-Application-NET.Server/src/statistics/UserStatisticsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Library_Web_Application_NET.Server.src.statistics
+{
+    public class UserStatisticsDto
+    {
+        public long NumberOfUsers { get; set; }
+
+        public long AvgNumberOfReservations { get; set; }
+
+        public double AvgReservationLength { get; set; }
+    }
+}
