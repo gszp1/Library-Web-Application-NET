9359c3a32d7d82e0c93dd37d95c22616fe724756
gszp1 <grzybekmateusz@poczta.fm>
2024-08-21 23:24:43 +0200

    Adds CountsPerMonthDto.

---
+++ Library-Web-Application-NET.Server/src/statistics/CountsPerMonthDto.cs
@@ -0,0 +1,29 @@
+﻿namespace Library_Web_Application_NET.Server.src.statistics
+{
+    public class CountsPerMonthDto
+    {
+        public long Jan { get; set; }
+
+        public long Feb { get; set; }
+
+        public long Mar { get; set; }
+
+        public long Apr { get; set; }
+
+        public long May { get; set; }
+
+        public long Jun { get; set; }
+
+        public long Jul { get; set; }
+
+        public long Aug { get; set; }
+
+        public long Sep { get; set; }
+
+        public long Oct { get; set; }
+
+        public long Nov { get; set; }
+
+        public long Dec { get; set; }
+    }
+}
