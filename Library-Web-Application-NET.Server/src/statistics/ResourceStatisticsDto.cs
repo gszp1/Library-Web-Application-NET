4f2986fde38dffb14656b087f1a3bc29f1aa221a
gszp1 <grzybekmateusz@poczta.fm>
2024-08-21 23:21:04 +0200

    Adds ResourceStatisticsDto.

---
+++ Library-Web-Application-NET.Server/src/statistics/ResourceStatisticsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Library_Web_Application_NET.Server.src.statistics
+{
+    public class ResourceStatisticsDto
+    {
+        public long NumberOfResources { get; set; }
+
+        public long NumberOfInstances { get; set; }
+
+        public long BorrowedInstances { get; set; }
+
+        public long ReservedInstances { get; set; }
+    }
+}
