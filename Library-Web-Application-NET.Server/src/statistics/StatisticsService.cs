f8767d602a175ba6fb8ed3a7508b263b85c62f44
gszp1 <grzybekmateusz@poczta.fm>
2024-08-23 07:50:42 +0200

    Adds GetUserStatisticsAsync implementations.

--- Library-Web-Application-NET.Server/src/statistics/StatisticsService.cs
+++ Library-Web-Application-NET.Server/src/statistics/StatisticsService.cs
@@ -2,6 +2,7 @@
 using Library_Web_Application_NET.Server.src.repository.interfaces;
 using Library_Web_Application_NET.Server.src.service;
 using Library_Web_Application_NET.Server.src.statistics.interfaces;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Library_Web_Application_NET.Server.src.statistics
 {
@@ -16,6 +17,25 @@ namespace Library_Web_Application_NET.Server.src.statistics
 
         public async Task<UserStatisticsDto> GetUserStatisticsAsync()
         {
+            long userCount = await unitOfWork.Users.CountUsersAsync();
+            List<Reservation> reservations = await unitOfWork.Reservations.FindAllAsync();
+            long reservationCount = reservations.Count();
+            long reservationsLengthSum = reservations.Select(r =>
+            {
+                if (r.ReservationEnd == null)
+                {
+                    return 0;
+                }
+                DateTime dateStart = r.ReservationStart.ToDateTime(TimeOnly.Parse("00:00AM"));
+                DateTime dateEnd = r.ReservationEnd.Value.ToDateTime(TimeOnly.Parse("00:00AM"));
+                return (dateEnd - dateStart).Days;
+            }).Sum();
+            return new UserStatisticsDto
+            {
+                NumberOfUsers = userCount,
+                AvgNumberOfReservations = (long)(userCount == 0 ? 0 : Math.Round((double)reservationCount / userCount, 0)),
+                AvgReservationLength = reservationsLengthSum == 0 ? 0 : (double)reservationsLengthSum / reservationCount
+            };
         }
 
         public async Task<ResourceStatisticsDto> GetResourceStatisticsAsync()
