b22e98d45710b9ea78c07ae1fbaab12bf00c6798
gszp1 <grzybekmateusz@poczta.fm>
2024-08-28 22:15:55 +0200

    Adds auth directory.

---
+++ Library-Web-Application-NET.Server/src/auth/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Library_Web_Application_NET.Server.src.model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Web_Application_NET.Server.src.auth
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly UserManager<User> userManager;
+
+        public AuthController(UserManager<User> userManager) {
+            this.userManager = userManager;
+        }
+
+        //[HttpPost("register")]
+        //public async Task<IActionResult> Register([FromBody])
+    }
+}
---
+++ Library-Web-Application-NET.Server/src/auth/RegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Library_Web_Application_NET.Server.src.auth
+{
+    public class RegisterRequest
+    {
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
