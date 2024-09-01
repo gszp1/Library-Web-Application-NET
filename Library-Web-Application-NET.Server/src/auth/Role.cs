34a7a2b65586e6a49022f1b25714aa41494d7607
gszp1 <grzybekmateusz@poczta.fm>
2024-09-01 22:33:30 +0200

    Adds RoleHandler class.

--- Library-Web-Application-NET.Server/src/auth/Role.cs
+++ Library-Web-Application-NET.Server/src/auth/Role.cs
@@ -7,4 +7,36 @@ namespace Library_Web_Application_NET.Server.src.auth
         User,
         Admin
     }
+
+    static class RoleHandler
+    {
+        public static List<Permission> GetPermissions(Role role)
+        {
+            switch (role)
+            {
+                case Role.User:
+                    return
+                    [
+                        Permission.User_Create,
+                        Permission.User_Update,
+                        Permission.User_Delete,
+                        Permission.User_Read
+                    ];
+                case Role.Admin:
+                    return
+                    [
+                        Permission.Admin_Create,
+                        Permission.Admin_Update,
+                        Permission.Admin_Delete,
+                        Permission.Admin_Read,
+                        Permission.User_Read,
+                        Permission.User_Update,
+                        Permission.User_Delete,
+                        Permission.User_Create
+                    ];
+                default:
+                    return [];
+            }
+        }
+    }
 }
--- Library-Web-Application-NET.Server/src/auth/TokenService.cs
+++ Library-Web-Application-NET.Server/src/auth/TokenService.cs
@@ -1,12 +1,26 @@
 ﻿using Library_Web_Application_NET.Server.src.model;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Library_Web_Application_NET.Server.src.auth
 {
+
     public class TokenService : ITokenService
     {
+
+        private readonly IConfiguration configuration;
+
+        private readonly SymmetricSecurityKey key;
+
+        public TokenService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]));
+        }
+
         public string CreateToken(User user)
         {
-            throw new NotImplementedException();
+            var claims
         }
     }
 }
