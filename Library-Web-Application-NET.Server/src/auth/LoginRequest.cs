52e22f2d4811583e62f3677a85efb8630991efdc
gszp1 <grzybekmateusz@poczta.fm>
2024-09-02 01:29:35 +0200

    Adds annotations.

--- Library-Web-Application-NET.Server/src/auth/LoginRequest.cs
+++ Library-Web-Application-NET.Server/src/auth/LoginRequest.cs
@@ -1,9 +1,14 @@
-﻿namespace Library_Web_Application_NET.Server.src.auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_Web_Application_NET.Server.src.auth
 {
     public class LoginRequest
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
         public string Password { get; set; }
     }
 }
