779f5d7f32a09205c147e0e25c6b4475565dd82c
gszp1 <grzybekmateusz@poczta.fm>
2024-09-04 22:26:02 +0200

    Adds DbConfigurationController.

---
+++ Library-Web-Application-NET.Server/src/admin/DbConfigurationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Web_Application_NET.Server.src.admin
+{
+    [ApiController]
+    [Route("api/config")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class DbConfigurationController : ControllerBase
+    {
+    }
+}
