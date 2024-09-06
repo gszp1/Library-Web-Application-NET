d23e1ba3d598421b4213824721180676f377e6e8
gszp1 <grzybekmateusz@poczta.fm>
2024-09-06 02:21:46 +0200

    Adds dropping and recreating database.

--- Library-Web-Application-NET.Server/src/admin/DbConfigurationService.cs
+++ Library-Web-Application-NET.Server/src/admin/DbConfigurationService.cs
@@ -1,4 +1,7 @@
 ﻿using Library_Web_Application_NET.Server.src.data.context;
+using Library_Web_Application_NET.Server.src.exception;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace Library_Web_Application_NET.Server.src.admin
 {
@@ -23,10 +26,48 @@ namespace Library_Web_Application_NET.Server.src.admin
 
         public async Task CreateDatabaseWithExampleDataAsync()
         {
-            //await context.Database.EnsureDeletedAsync();
-            //await context.Database.EnsureCreatedAsync();
-            //await initializer.SeedAdminIntoDatabase(serviceProvider);
-            throw new NotImplementedException();
+            await context.Database.EnsureDeletedAsync();
+            ClearWwwRoot();
+
+            CopyFiles
+            (
+                Path.Combine(env.WebRootPath, "sampleImages"),
+                Path.Combine(env.WebRootPath, "images")
+            );
+            CopyFiles
+            (
+                Path.Combine(env.WebRootPath, "sampleUserImages"),
+                Path.Combine(env.WebRootPath, "userImages")
+            );
+
+            await context.Database.EnsureCreatedAsync();
+            string scriptPath = Path.Combine(env.WebRootPath, "sql", "LibraryDbScriptData.sql");
+            string script = await File.ReadAllTextAsync(scriptPath);
+
+            using (var connection = new SqlConnection(context.Database.GetDbConnection().ConnectionString))
+            {
+                await connection.OpenAsync();
+                SqlCommand command = connection.CreateCommand();
+                SqlTransaction transaction = connection.BeginTransaction();
+                command.Connection = connection;
+                command.Transaction = transaction;
+
+                try
+                {
+                    foreach (string batch in script.Split(new string[] { "GO" }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        command.CommandText = batch;
+                        await command.ExecuteNonQueryAsync();
+                    }
+                    transaction.Commit();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    transaction.Rollback();
+                    throw new OperationFailedException("Failed to recreate database with data.");
+                }
+            }
         }
 
         public async Task CreateEmptyDatabaseAsync()
@@ -60,6 +101,33 @@ namespace Library_Web_Application_NET.Server.src.admin
                 }
             }
 
+        }
+
+        private void CopyFiles(string src, string dst)
+        {
+            try
+            {
+                if (!Directory.Exists(src))
+                {
+                    return;
+                }
+                if (!Directory.Exists(dst))
+                {
+                    Directory.CreateDirectory(dst);
+                }
+                DirectoryInfo dirInfo = new DirectoryInfo(src);
+                FileInfo[] files = dirInfo.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    string destFilePath = Path.Combine(dst, file.Name);
+
+                    file.CopyTo(destFilePath, overwrite: true);
+                }
+            }
+            catch (Exception)
+            {
+                return;
+            }
         }
     }
 }
